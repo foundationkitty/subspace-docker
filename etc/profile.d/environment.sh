048c6250da5e4d4d90512267dc52d721186ce972
foundationkitty <45774850+foundationkitty@users.noreply.github.com>
2019-11-07 13:59:13 -0800

    Create environment.sh

---
+++ etc/profile.d/environment.sh
@@ -0,0 +1,14 @@
+export DOMAIN=
+export PUID=
+export PGID=
+export ACME_EMAIL=
+export CF_EMAIL=
+export CF_API_KEY=
+export OAUTH_CLIENT_ID=
+export OAUTH_CLIENT_SECRET=
+export OAUTH_SECRET=
+export OAUTH_WHITELIST=
+export MYSQL_PASSWORD=
+export APPDATA=
+export APPDATA_WEB=
+export APPDATA_GAMES=
